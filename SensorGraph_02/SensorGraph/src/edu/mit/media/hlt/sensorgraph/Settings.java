2c18df8f4a1ff727dbb41dd5ecbebb0354fd39ae
Eugene Lee <eugene.lee337@gmail.com>
2013-04-22 01:21:14 -0400

    adding in karenas shit

--- SensorGraph_02/SensorGraph/src/edu/mit/media/hlt/sensorgraph/Settings.java
+++ SensorGraph_02/SensorGraph/src/edu/mit/media/hlt/sensorgraph/Settings.java
@@ -3,6 +3,12 @@ package edu.mit.media.hlt.sensorgraph;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class Settings extends Activity {
 
@@ -10,6 +16,7 @@ public class Settings extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_settings);
+		addListenerOnButton();
 	}
 
 	@Override
@@ -17,6 +24,29 @@ public class Settings extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.settings, menu);
 		return true;
+	}
+	
+	public void addListenerOnButton() {
+		
+		final RadioGroup radioSexGroup = (RadioGroup) findViewById(R.id.radioSex);
+		Button btnDisplay = (Button) findViewById(R.id.btnDisplay);
+	 
+		btnDisplay.setOnClickListener(new OnClickListener() {
+	 
+			@Override
+			public void onClick(View v) {
+	 
+			        // get selected radio button from radioGroup
+				int selectedId = radioSexGroup.getCheckedRadioButtonId();
+	 
+				// find the radiobutton by returned id
+			        RadioButton radioSexButton = (RadioButton) findViewById(selectedId);
+	 
+				Toast.makeText(Settings.this,
+					radioSexButton.getText(), Toast.LENGTH_SHORT).show();
+	 
+			}
+		});
 	}
 
 }
