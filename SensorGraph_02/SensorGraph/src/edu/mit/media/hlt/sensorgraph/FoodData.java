1e91e3c9b3fd135997fdfaa3f003372229703d43
Eugene Lee <eugene.lee337@gmail.com>
2013-04-22 22:19:34 -0400

    Added buttons to graph

--- SensorGraph_02/SensorGraph/src/edu/mit/media/hlt/sensorgraph/FoodData.java
+++ SensorGraph_02/SensorGraph/src/edu/mit/media/hlt/sensorgraph/FoodData.java
@@ -2,13 +2,19 @@ package edu.mit.media.hlt.sensorgraph;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
 
 public class FoodData extends Activity {
 
+	private ImageView imageview;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		imageview = (ImageView)findViewById(R.id.imageView);
 		setContentView(R.layout.activity_food_data);
 	}
 
@@ -18,5 +24,20 @@ public class FoodData extends Activity {
 		getMenuInflater().inflate(R.menu.food_data, menu);
 		return true;
 	}
+	
+	 /** Called when the user clicks the Activity Data Button */
+    public void respondToDayButton () {
+    	imageview.setImageResource(R.drawable.activity_day_graph);
+    }
+    
+    /** Called when the user clicks the Activity Data Button */
+    public void respondToWeekButton () {
+    	imageview.setImageResource(R.drawable.activity_week_graph);
+    }
+    
+	 /** Called when the user clicks the Activity Data Button */
+    public void respondToMonthButton () {
+    	imageview.setImageResource(R.drawable.activity_month_graph);
+    }
 
 }
